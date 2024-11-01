f0a843b0d50dc5aa82a74fa7c7f2016b55081d99
Roach2197 <roachsportscards@gmail.com>
2024-11-01 17:49:53 -0400

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx'; // Make sure this matches your file structure
+import App from './App'; // Make sure this matches your file structure
 
 ReactDOM.render(
   <React.StrictMode>
