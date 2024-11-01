b0a6dc95ebd999cc7efa880d6c4985b022db6bf5
Glitch (glitch-hello-react) <none>
2024-11-01 18:25:39 +0000

    🏖🛳 Checkpoint ./vite.config.js:71533548/464

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'; // Importing the function to define the config
+import react from '@vitejs/plugin-react'; // Importing the React plugin
 
-export default defineConfig({
-  plugins: [react()],
-});
+// Exporting the Vite configuration
+export default defineConfig({ // Start of object
+  plugins: [react()], // The plugins array with the React plugin
+}); // End of object
